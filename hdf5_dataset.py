44ee05b9e4a18487442f8f09a7e2a6d4d986d94a
Andrin Jenal <ajenal@ethz.ch>
2017-02-22 08:55:04 +0100

    start support celeb dataset

--- hdf5_dataset.py
+++ hdf5_dataset.py
@@ -1,10 +1,11 @@
 import os
 import collections
-from scipy import misc
+from scipy import misc, ndimage
 import numpy as np
 import h5py
-
+from zipfile import ZipFile
 from time import time
+import tqdm
 
 Datasets = collections.namedtuple('Datasets', ['train', 'validation'])
 
@@ -112,7 +113,57 @@ def find_file(data_path, extensions):
         raise IOError
 
 
-def test_load_dataset():
+class CelebDataset:
+
+    def __init__(self, dataset_dir, image_size=64, channels=1):
+        self.dataset_dir = dataset_dir
+        self.image_size = image_size
+        self.channels = channels
+
+    def create_dataset_from_zip(self, path_to_zip_file, dataset_filename="celeb_dataset.h5"):
+        images = []
+        image_names = []
+        with ZipFile(path_to_zip_file, 'r') as zfile:
+            file_list = zfile.namelist()
+
+            for img_file in file_list:
+                if str(img_file).endswith('.jpg'):
+                    with zfile.open(img_file) as imf:
+                        img = misc.imread(imf)
+                        image = self.get_normalized_image(img, self.image_size, self.image_size)
+                        if self.channels == 1:
+                            image = self.image2gray(image)
+                        images.append(image)
+                        image_names.append(img_file)
+
+        file_name_path = os.path.join(self.dataset_dir, dataset_filename)
+        with h5py.File(file_name_path, 'a') as hfile:
+            self.save_images_to_hdf5(hfile, zip(image_names, images))
+
+    def resize_width(self, image, width=64.):
+        h, w = np.shape(image)[:2]
+        return misc.imresize(image, [int((float(h) / w) * width), width])
+
+    def center_crop(self, x, height=64):
+        h = np.shape(x)[0]
+        j = int(round((h - height) / 2.))
+        return x[j:j + height, :, :]
+
+    def get_normalized_image(self, img, width=64, height=64):
+        return self.center_crop(self.resize_width(img, width=width), height=height)
+
+    def save_images_to_hdf5(self, open_h5file, image_list):
+        for img_name, img_data in image_list:
+            dataset = open_h5file.create_dataset(self.get_filename(img_name), data=img_data, shape=img_data.shape)
+
+    def get_filename(self, path):
+        return os.path.splitext(os.path.basename(path))[0]
+
+    def image2gray(self, image):
+        return image[:, :, 0] * 0.299 + image[:, :, 1] * 0.587 + image[:, :, 2] * 0.114
+
+
+def test_HDF5_Dataset():
     print('load dataset')
     start_time = time()
     data_set = read_data_set('/home/ajenal/Documents/masterthesis/project/tbone/tree_all_28k_2k_1v_skel_64x64_inverted.zip', image_size=64, shape=(64,64))
@@ -131,6 +182,11 @@ def test_load_dataset():
     elapsed_time = '%.3f' % (end_time - start_time)
     print('Elapsed time: ' + elapsed_time + ' seconds\n')
 
+
+def test_CelebDataset():
+    c = CelebDataset('/home/ajenal/', image_size=64)
+    c.create_dataset_from_zip('/home/ajenal/celeb_imgs.zip')
+
 if __name__ == '__main__':
-    test_load_dataset()
-    print(find_file('/home/ajenal/Documents/masterthesis/project/tbone/'))
+    #test_HDF5_Dataset()
+    test_CelebDataset()
